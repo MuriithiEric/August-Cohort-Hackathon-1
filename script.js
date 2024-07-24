a0c0ebb2cad5cdabdc43b6888f7a88b84708923e
johnnytash <thiongoj959@gmail.com>
2024-07-24 18:14:59 +0300

    Making navbar to be fixed and adding slide effect on the into heading

--- script.js
+++ script.js
@@ -126,8 +126,16 @@ document.addEventListener("DOMContentLoaded", () => {
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
+    const targetId = this.getAttribute("href");
+    if (targetId && targetId !== "#") {
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    }
   });
 });
+
+
